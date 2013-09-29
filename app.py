ec85656f20fcb1db45b8157f7eef80de519340e0
Aamir Mansoor <me@aam1r.com>
2013-09-29 17:16:25 -0400

    Make Ratings work with current Media
    Move Ratings from dummy Parse app to existing app. Ratings are now saved
    in a new Rating class, which contains the following info:
    
      * mediaId
      * rating
      * default Parse columns
    
    Additionally, lookups are now done based on the photo's objectId instad
    of the Instagram photo URL.

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from __future__ import division
 from flask import Flask, request, render_template, json, url_for, redirect, session
-from parse_rest.connection import register
+from parse import Media, Parse, Rating
+from parse_rest.connection import ParseBatcher
 from parse_rest.query import QueryResourceDoesNotExist
 from random import randint
-from rating import Photo, BASE_RATING, KC, WIN, LOSS
+from rating import BASE_RATING, KC, WIN, LOSS
 
-import urllib, urllib2, os, urlparse, time
+import ast, urllib, urllib2, os, urlparse, time
 
 app = Flask(__name__)
 app.secret_key = 'some_random_secret'
+parse = Parse()
 
 INSTAGRAM_CLIENT_ID = 'd18a0312ac06430cba43f02ccbf9c5d4'
 INSTAGRAM_CLIENT_SECRET = '7a2040afd3044962a636ac001be5f5a2'
@@ -58,53 +60,60 @@ def logged_in():
 
 @app.route("/tally_round", methods=['POST'])
 def tally_round():
-    photo1_raw = None
-    photo2_raw = None
+    photo1 = None
+    photo2 = None
     result = None
 
     if 'photo1' in request.form and 'photo2' in request.form and 'result' in request.form:
-        photo1_raw = request.form['photo1']
-        photo2_raw = request.form['photo2']
+        photo1 = request.form['photo1']
+        photo2 = request.form['photo2']
         result = request.form['result']
 
         # validate POST data for security
-        photo1_url = urlparse.urlparse(photo1_raw)
-        photo2_url = urlparse.urlparse(photo2_raw)
-
-        if photo1_url.scheme != 'http' or photo1_url.query != '' or photo1_url.params != '':
-            return json.dumps({"errorMsg": "Invalid data"}), 400
-        if photo2_url.scheme != 'http' or photo2_url.query != '' or photo2_url.params != '':
+        if len(photo1) == 0 or len(photo2) == 0:
             return json.dumps({"errorMsg": "Invalid data"}), 400
         if result != 'win' and result != 'loss':
             return json.dumps({"errorMsg": "Invalid data"}), 400
     else:
         return json.dumps({"errorMsg": "Invalid data"}), 400
 
-    print photo1_raw, photo2_raw, result
+    print photo1, photo2, result
 
+    # verify that media exists
     try:
-        photo1 = Photo.Query.get(img_url=photo1_raw)
-        photo2 = Photo.Query.get(img_url=photo2_raw)
+        photo1 = Media.Query.get(objectId=photo1)
+        photo2 = Media.Query.get(objectId=photo2)
     except QueryResourceDoesNotExist:
         return json.dumps({"errorMsg": "Invalid data"}), 400
 
-    ratings = update_rating(photo1, photo2, result)
-    photo1.rating = ratings[0]
-    photo2.rating = ratings[1]
+    rating1 = None
+    rating2 = None
 
-    photos = [photo1, photo2]
+    # if rating exists already, update it
+    try:
+        rating1 = Rating.Query.get(mediaId=photo1.objectId)
+        rating2 = Rating.Query.get(mediaId=photo2.objectId)
+    except QueryResourceDoesNotExist:
+        rating1 = Rating(rating=BASE_RATING, mediaId=photo1.objectId)
+        rating2 = Rating(rating=BASE_RATING, mediaId=photo2.objectId)
+
+    ratings = update_rating(rating1, rating2, result)
+    rating1.rating = ratings[0]
+    rating2.rating = ratings[1]
+
+    ratings = [rating1, rating2]
     try:
         batcher = ParseBatcher()
-        batcher.batch_save(photos)
+        batcher.batch_save(ratings)
     except:
-        print "ERROR failed to save vote!"
+        return json.dumps({"errorMsg": "Failed to save votes"}), 400
 
     return json.dumps({"success": True}), 200
 
 
-def update_rating(photo1, photo2, result):
-    pl_one = photo1.rating
-    pl_two = photo2.rating
+def update_rating(rating1, rating2, result):
+    pl_one = rating1.rating
+    pl_two = rating2.rating
 
     eA = 1 / (1 + 10**((pl_two - pl_one)/400))
     eB = 1 / (1 + 10**((pl_one - pl_two)/400))
@@ -114,36 +123,42 @@ def update_rating(photo1, photo2, result):
     if result == 'win':
         pl_one = pl_one + KC*(WIN - eA)
         pl_two = pl_two + KC*(LOSS - eB)
-        print "pl_one: ", pl_one
-        print "pl_two: ", pl_two
+        print "pl_one: %d, pl_two: %d" % (pl_one, pl_two)
     if result == 'loss':
         pl_one = pl_one + KC*(LOSS - eA)
         pl_two = pl_two + KC*(WIN - eB)
-        print "pl_one: ", pl_one
-        print "pl_two: ", pl_two
+        print "pl_one: %d, pl_two: %d" % (pl_one, pl_two)
 
     return [pl_one, pl_two]
 
 
 @app.route("/new_round")
 def new_round():
-    players = get_new_players()
+    new_players = get_new_players()
+    players = []
 
-    print "players: ", players[0].img_url, players[1].img_url
-    return json.dumps([players[0].img_url, players[1].img_url])
+    for player in new_players:
+      images = ast.literal_eval(player.images)
+      players.append({'objectId': player.objectId, 'images': images})
+
+    print "players: ", players[0]['objectId'], players[1]['objectId']
+    return json.dumps(players)
 
 
 """ Returns a list containing two new players """
 def get_new_players():
-    all = Photo.Query.all()
-    count = all.count()
+    all = Media.Query.all()
+    count = 100 # all.count()
     rand1 = randint(0, count - 1)
     rand2 = randint(0, count - 1)
+
     while rand1 == rand2:
         rand2 = randint(0, count - 1)
+
     qs1 = all.limit(1).skip(rand1)
     qs2 = all.limit(1).skip(rand2)
     players = []
+
     for item in qs1:
         players.append(item)
 
@@ -155,28 +170,11 @@ def get_new_players():
 
 @app.route("/rating")
 def rating():
-    photo_urls = ['http://distilleryimage5.s3.amazonaws.com/8ba48b0a283c11e3aa8c22000a1fc809_7.jpg',
-                  'http://distilleryimage7.s3.amazonaws.com/5ecb6cf028b611e3b16122000a1f9e61_7.jpg',
-                  'http://distilleryimage2.s3.amazonaws.com/fa569a4828b411e3948222000a1f9307_8.jpg',
-                  'http://distilleryimage3.s3.amazonaws.com/e172b9ac28b511e3ab9022000a9f14bb_7.jpg',
-                  'http://distilleryimage5.s3.amazonaws.com/27bfdfde28b611e387d622000aeb0b75_7.jpg',
-                  'http://distilleryimage8.s3.amazonaws.com/0ffe132828b311e3ab3e22000a1fb352_7.jpg',
-                  'http://distilleryimage3.s3.amazonaws.com/2217cbd228b611e3a0fd22000aa8039a_8.jpg',
-                  'http://distilleryimage4.s3.amazonaws.com/5367aa4a28b611e3a1ff22000a1fcee4_7.jpg',
-                  'http://distilleryimage10.s3.amazonaws.com/278be1ac28b611e3872722000a1fd26f_7.jpg',
-                  'http://distilleryimage1.s3.amazonaws.com/5b488e54285311e3b30422000aaa0585_7.jpg']
-
-
-    for url in photo_urls:
-        try:
-            aPhoto = Photo.Query.get(img_url=url)
-        except:
-            aPhoto = Photo(img_url=url, rating=BASE_RATING)
-            aPhoto.save()
-
     return render_template('ratingtest.html')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.debug = True
     app.run(host='0.0.0.0', port=port)
+    parse = Parse()
--- rating.py
+++ rating.py
@@ -1,15 +1,5 @@
-from parse_rest.connection import register
-from parse_rest.datatypes import Object
-
 # Elo Rating System constants
 BASE_RATING = 1400
 KC = 32
 WIN = 1
 LOSS = 0
-
-class Photo(Object):
-    pass
-
-
-# parse for dev purposes
-register('jWkR2kmHwh45o14jXDmoA9ujE7T5LVGfLPnaWwN0', '3yENrtbiaiu3k934sehtjq8VMNiTrzZo6Za8J5ob')
