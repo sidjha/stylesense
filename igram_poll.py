02921d08b4b0bce973c088c18a3fae39bb7dba6c
Aamir Mansoor <me@aam1r.com>
2013-09-29 03:29:05 -0400

    Poll Instagram API and store information in Parse
    - Add Instagram and Parse modules
    - Write a standalone script (that should be running in background at all
      times) that:
        * Queries Instagram /tag/recent endpoint for "#ootd"
        * Filters out users based on some thresholds
        * Serializes data and saves on Parse

---
+++ igram_poll.py
@@ -0,0 +1,75 @@
+from app import INSTAGRAM_CLIENT_ID, INSTAGRAM_CLIENT_SECRET
+from instagram.client import InstagramAPI
+from parse import Media, Parse
+
+import time
+
+POLL_DELAY = 5 # in seconds
+FOLLOWER_THRESHOLD = 200
+
+api = None
+parse = None
+
+def preprocess_obj(obj):
+  # add followers to object
+  user = api.user(obj.user.id)
+  followers = user.counts['followed_by']
+  obj.followers = followers
+
+  # verify 'tags' exist
+  if not hasattr(obj, 'tags'):
+    obj.tags = []
+
+  return obj
+
+def poll():
+  recent_tags = api.tag_recent_media(tag_name="ootd")
+  recent_media = recent_tags[0]
+  n = 0
+
+  print "Processing %d recent media items" % len(recent_media)
+
+  for obj in recent_media:
+    obj = preprocess_obj(obj)
+
+    details = {'caption': obj.caption,
+               'commentCount': obj.comment_count,
+               'comments': obj.comments,
+               'filter': obj.filter,
+               'followers': obj.followers,
+               'images': obj.images,
+               'instagramId': obj.id,
+               'likeCount': obj.like_count,
+               'likes': obj.likes,
+               'link': obj.link,
+               'standardResolutionUrl': obj.get_standard_resolution_url(),
+               'tags': obj.tags,
+               'uploadTime': obj.created_time,
+               'userId': obj.user.id }
+
+    media = Media(details)
+
+    if media.followers < FOLLOWER_THRESHOLD:
+      continue
+    elif 'ootd' not in media.tags:
+      continue
+
+    media.save()
+    n += 1
+
+    if media.objectId:
+      print "Media saved successfully (id: %s)" % media.objectId
+
+  print "%d items saved" % n
+
+
+if __name__ == "__main__":
+  print "Starting Instagram poller. Poll delay: %d" % POLL_DELAY
+
+  api = InstagramAPI(INSTAGRAM_CLIENT_ID, INSTAGRAM_CLIENT_SECRET)
+  parse = Parse()
+
+  while True:
+    poll()
+    print '-' * 20
+    time.sleep(POLL_DELAY)
---
+++ parse.py
@@ -0,0 +1,78 @@
+from parse_rest.connection import register
+from parse_rest.datatypes import Object
+from parse_rest.query import QueryResourceDoesNotExist
+
+import json, httplib, urllib
+
+PARSE_APPLICATION_ID = 'GGEI1UGiW8CqKXqovHbt4w9ajoJIVZl7N2uJwdDB'
+PARSE_REST_API_KEY = 'ihituXdUkmnFDZaOu1M44FC4GFQ82ryKZkTx6KYO'
+
+# Used to connect to Parse
+class Parse:
+  def __init__(self):
+    register(PARSE_APPLICATION_ID, PARSE_REST_API_KEY)
+
+
+# Stores image data retrieved from Instagram
+class Media(Object):
+  def __init__(self, details):
+    for k, v in details.items():
+      v = self._serialize_data(k, v)
+      setattr(self, k, v)
+
+  def exists(self):
+    connection = httplib.HTTPSConnection('api.parse.com', 443)
+    params = urllib.urlencode({"where":json.dumps({
+          "instagramId": self.instagramId
+        })})
+    connection.connect()
+    connection.request('GET', '/1/classes/Media?%s' % params, '', {
+          "X-Parse-Application-Id": PARSE_APPLICATION_ID,
+          "X-Parse-REST-API-Key": PARSE_REST_API_KEY,
+        })
+
+    result = json.loads(connection.getresponse().read())
+    return len(result['results']) > 0
+
+
+  def save(self):
+    exists = self.exists()
+
+    if exists:
+      print "%s already exists" % self.instagramId
+    else:
+      super(Media, self).save()
+
+  def _serialize_data(self, k, v):
+    if k == 'caption':
+      if hasattr(v, 'text'):
+        v = v.text
+      else:
+        v = ''
+    elif type(v) is list:
+      serialized = map(self._serialize_list, v)
+      v = serialized
+    elif type(v) is dict:
+      serialized = self._serialize_dict(v)
+      v = str(serialized)
+
+    return v
+
+  def _serialize_dict(self, obj):
+    serialized = {}
+
+    for k, v in obj.items():
+      if hasattr(v, 'url'):
+        return {'height': v.height,
+                'url': v.url,
+                'width': v.width }
+      else:
+        return repr(v)
+
+    return serialized
+
+  def _serialize_list(self, obj):
+    if hasattr(obj, 'name'):
+      return obj.name
+    else:
+      return repr(obj)
