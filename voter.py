eec23e8ed5fdbf0f05ec69f7a5f0828c5d8da64a
Siddharth Jha <sid@sidjha.com>
2013-10-03 15:59:20 -0400

    voter script v2

--- voter.py
+++ voter.py
@@ -1,19 +1,34 @@
-from parse import Media, Parse
+from parse import Media, Parse, Rating
 from app import parse
-from parse_rest.connection import ParseBatcher
+from apputils import LinkedRand, parse_save_batch_objects
+from rating import hot
 
 def run_bot():
 	photos = []
 
 	count = Media.Query.all().count()
 	dataset_size = int(count*0.05)
-	dataset = Media.Query.all().limit(dataset_size)
+
+	randint = LinkedRand(count)
+	rand = randint()
+	dataset = Media.Query.all().skip(rand).limit(dataset_size)
 
 	for photo in dataset:
-		photo.wins = photo.wins + 1
+		randint = LinkedRand(2)
+		rand = randint()
+		if rand == 0:
+			photo.wins = photo.wins + 1
+		else:
+			photo.losses = photo.losses + 1
 		photos.append(photo)
+	print photos
 
-	try:
-		ParseBatcher.batch_save(photos)
-	except e:
-		print "ERROR: " + e
+	parse_save_batch_objects(photos)
+
+	ratings = []
+	for photo in photos:
+		rating = Rating.Query.get(mediaId=photo.objectId)
+		rating.rating = hot(photo.wins, photo.losses, photo.createdAt)
+		ratings.append(rating)
+
+	parse_save_batch_objects(ratings)
