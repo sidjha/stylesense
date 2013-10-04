5029f8a582f0d187a65de2651ee21f66fa415e5b
Siddharth Jha <sid@sidjha.com>
2013-10-04 05:49:16 -0400

    A script that generates stats: most skipped, wins and losses.

---
+++ stats.py
@@ -0,0 +1,67 @@
+from parse import Media, Skip
+from app import parse
+
+def most_skipped():
+	QUERY_SIZE = 5
+	mostskipped = Skip.Query.all().limit(QUERY_SIZE).order_by("-skips")
+	skipped_media = []
+	for item in mostskipped:
+		media = Media.Query.get(objectId=item.mediaId)
+		skipped_media.append({'media': media, 'skips': item.skips})
+
+	print "Most skipped pictures (descending)"
+	for i in range(0,20):
+		print "-",
+	print "\r"
+
+	for item in skipped_media:
+		media = item['media']
+		skips = item['skips']
+		print skips, " skips: ", media.link, " by ", media.username
+	print "\r"
+
+	return skipped_media
+
+def most_wins():
+	QUERY_SIZE = 5
+	mostwins = Media.Query.all().limit(5).order_by('-wins')
+
+	print "Pictures with most wins (descending)"
+	for i in range(0,20):
+		print "-",
+	print "\r"
+
+	for photo in mostwins:
+		print photo.wins, " wins: ", photo.link, " by ", photo.username
+	print "\r"
+	
+	return mostwins
+
+def most_losses():
+	QUERY_SIZE = 5
+	mostlosses = Media.Query.all().limit(5).order_by('-losses')
+
+	print "Pictures with most losses (descending)"
+	for i in range(0,20):
+		print "-",
+	print "\r"
+
+	for photo in mostlosses:
+		print photo.losses, " losses: ", photo.link, " by ", photo.username
+	print "\r"
+	
+	return mostlosses
+
+if __name__ == '__main__':
+	for i in range(0,20):
+		print "-",
+	print "\r"
+	print "STYLESENSE STATS FOR TODAY"
+	for i in range(0,20):
+		print "-",
+	print "\r"
+
+	most_skipped()
+	most_wins()
+	most_losses()
+	print "\r"
